fd54b64fe6160a3b3f2746fec40ccbbd77800951
lwn314 <lwn314@163.com>
2018-01-24 10:56:11 +0800

    ffmpeg demo

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,48 @@
+#include <jni.h>
+#include <string>
+
+extern "C" {
+//编码
+#include "libavcodec/avcodec.h"
+//封装格式处理
+#include "libavformat/avformat.h"
+//像素处理
+#include "libswscale/swscale.h"
+}
+
+extern "C"
+
+JNIEXPORT jstring JNICALL
+Java_com_mofic_fftest_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
+
+    char info[40000] = {0};
+
+    av_register_all();
+
+    AVCodec *c_temp = av_codec_next(NULL);
+
+    while (c_temp != NULL) {
+        if (c_temp->decode != NULL) {
+            sprintf(info, "%s[Dec]", info);
+        } else {
+            sprintf(info, "%s[Enc]", info);
+        }
+        switch (c_temp->type) {
+            case AVMEDIA_TYPE_VIDEO:
+                sprintf(info, "%s[Video]", info);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                sprintf(info, "%s[Audio]", info);
+                break;
+            default:
+                sprintf(info, "%s[Other]", info);
+                break;
+        }
+        sprintf(info, "%s[%10s]\n", info, c_temp->name);
+
+
+        c_temp = c_temp->next;
+    }
+    //LOGE("%s", info);
+    return env->NewStringUTF(info);
+}
